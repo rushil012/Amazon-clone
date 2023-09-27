8a97796f7f832c08520535846a69067964b5324d
rushil012 <111217694+rushil012@users.noreply.github.com>
2023-09-27 16:08:22 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,50 @@
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+   cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  
+  },{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:1
+  }]
+};
+
+
+function saveTostorage() {
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
+
+
+export function addtocart(productId) {
+  let matchingItem;
+    cart.forEach((cartitem) => {
+      if ((productId) == cartitem.productId) {
+        matchingItem = cartitem;
+      }
+    })
+    if (matchingItem) {
+      matchingItem.quantity += Number(document.querySelector(`.js-quantity-selector-${productId}`).value) ;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: Number(document.querySelector(`.js-quantity-selector-${productId}`).value) 
+      })
+    }
+
+    saveTostorage();
+}
+
+
+export function removefromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartitem) => {
+    if (cartitem.productId !== productId) {
+      newCart.push(cartitem);
+    }
+  });
+
+  cart = newCart;
+  saveTostorage();
+}
